1c7143374edf0d75de5e04b40770383c846cdcc3
Anuj <anujsaheba@gmail.com>
2019-02-06 14:14:16 -0600

    update

--- memory-game/src/MemoryCard.js
+++ memory-game/src/MemoryCard.js
@@ -4,11 +4,33 @@ import './MemoryCard.css';
 
 
 class MemoryCard extends Component{
+    constructor(props){
+    super(props);
+        this.state = {isFlipped:false};
+
+
+
+    } // End of Constructor Function
+    clickHandler(){
+        this.setState({
+            isFlipped: !this.state.isFlipped
+        }) // End of Set State
+    } // End of click Handler
 
     render(){
+
+        var cardClass = "MemoryCardInner";
+		if (this.state.isFlipped) {
+		    cardClass += " flipped";
+		}
+        
         return(
-            <div class = "MemoryCard">
-                {/* <img class="DC_image" alt="description" src="https://cdn.auth0.com/blog/optimizing-react/logo.png"></img> */}
+            <div className = "MemoryCard" onClick={(e)=>this.clickHandler(this)}>
+                {/* <img alt="test" src="https://media.istockphoto.com/vectors/houston-texas-linear-emblem-design-for-t-shirts-travel-stickers-and-vector-id860711778"></img> */}
+                <div className = {cardClass}>
+                    <div className = "MemoryCardBack"></div>
+                    <div className = "MemoryCardFront">∆</div>
+                </div>
             </div>
         )
     }
