cdeb54ce6166a46506fb517c9552c11094c8c096
Anuj <anujsaheba@gmail.com>
2019-02-07 18:50:02 -0600

    update

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -2,8 +2,50 @@ import React, { Component } from 'react';
 import './App.css';
 import MemoryCard from './MemoryCard.js';
 
+
+generateDeck(){
+  
+  symbols = [a, ß, £, §, •, $, +, ø];
+  deck = [];
+
+  for(var i = 1; i <= 16, i++){
+    card = {
+      isFlipped: false,
+      Symbol: symbols[i%8];
+    } // End of Card Object
+    deck.push(card);
+    shuffle(deck)
+    return deck
+  }; // End of For Loop
+}; // End of generateDeck function
+
+// Shuffle function
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  return a;
+}
+
+
+
 class App extends Component {
+  constructor(props){
+    super(props);
+      this.deck = generateDeck();
+      this.pickedCards = [];
+  }
+
   render() {
+    var cardsJSX = this.state.deck.map((card, index)=>{
+      return <MemoryCard />
+    });
+
+    
     return (
       <div className="App">
         <header className="App-header">
