bd3779a00bcf8b4a0cdbfed4a298f48dbffda4e6
Anuj <anujsaheba@gmail.com>
2019-02-11 18:55:13 -0600

    updated addContact

--- contacts-app/src/App.js
+++ contacts-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AddContact from './components/AddContact'
 
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div>
         <h1>Hello World</h1>
+        <AddContact />
       </div>
     );
   }
--- contacts-app/src/components/AddContact.js
+++ contacts-app/src/components/AddContact.js
@@ -11,13 +11,28 @@ class AddContact extends React.Component {
             addressVal: "",
             cityVal: "",
             stateVal: "",
-            zipCode: 12345
+            zipCodeVal: 12345
         }
         
     }
 
+    handleChange(e){
+        this.setState({
+            nameVal: e.target.nameVal,
+            emailVal: e.target.emailVal,
+            phoneVal: e.target.phoneVal,
+            addressVal: e.target.addressVal,
+            cityVal: e.target.cityVal,
+            stateVal: e.target.stateVal,
+            zipCodeVal: e.target.zipCodeVal
+       
+        })
+
+
+    }
 
     handleSubmit(){
+        alert('The contact information you have provided has been submitted')
 
     }
 
@@ -25,18 +40,14 @@ class AddContact extends React.Component {
         return (
             <div>
                 <form onSubmit={this.handleSubmit.bind(this)}>
-                     <label>Name:<input type="text" value={this.state.nameVal}/></label>
-                     <label>Email:<input type="text" value={this.state.emailVal}/></label>
-                     <label>Phone Number:<input type="text" value={this.state.phoneVal}/></label>
-                     <label>Address:<input type="text" value={this.state.addressVal}/></label>
-                     <label>City:<input type="text" value={this.state.cityVal}/></label>
-                     <label>State:<input type="text" value={this.state.stateVal}/></label>
-                     <label>Zip Code:<input type="number" value={this.state.zipCode}/></label>
-
-                     <input type = "submit" value = "Submit" /> 
-
-
-
+                    <label>Name:<input type="text" value={this.state.nameVal} onChange={this.handleChange.bind(this)}/></label> <br/>
+                    <label>Email:<input type="text" value={this.state.emailVal} onChange={this.handleChange.bind(this)}/></label> <br/>
+                    <label>Phone Number:<input type="text" value={this.state.phoneVal} onChange={this.handleChange.bind(this)}/></label> <br/>
+                    <label>Address:<input type="text" value={this.state.addressVal} onChange={this.handleChange.bind(this)}/></label> <br/>
+                    <label>City:<input type="text" value={this.state.cityVal} onChange={this.handleChange.bind(this)}/></label> <br/>
+                    <label>State:<input type="text" value={this.state.stateVal} onChange={this.handleChange.bind(this)}/></label> <br/>
+                    <label>Zip Code:<input type="number" value={this.state.zipCodeVal} onChange={this.handleChange.bind(this)}/></label> <br/>
+                    <input type = "submit" value = "Add Contact" /> 
 
                 </form>
 
